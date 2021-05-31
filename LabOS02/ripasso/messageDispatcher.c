3f8ec2d0f7df09fe016fe04b8211e7eab3436ae1
carlo bottaro <29379533+bottarocarloo@users.noreply.github.com>
2021-05-31 11:31:56 +0200

    message dispatcher

--- LabOS02/ripasso/messageDispatcher.c
+++ LabOS02/ripasso/messageDispatcher.c
@@ -14,18 +14,48 @@
 #define RED "\033[0;31m"
 #define GREEN "\033[32m"
 #define DF "\033[0m"
-#define MAX_DEPHT 5
+#define MAX 5
+//0 -> false
+//1 -> true
 
-struct msg_buffer
-{
-    char payload[3];
-    int type;
-}msg;
+int numChildren;
+int pid[MAX];
 
+int isString(char *buffer){
+    while(buffer[i]=='\n'){
+        if(buffer[i]<=0 || buffer[i]>=9){
+            return 1;
+        }
+    }
+    return 0;
+}
+
+void ctrlc(int sigNum){
+    for(int i =1 ;i<numChildren;i++){
+        kill(pid[i],SIGTERM);
+        printf("%s[%d] killed %s\n",RED,pid[i],DF);
+    }
+    printf("%s[MAIN][%d] killed %s\n",RED,pid[i],DF)
+}
 
 int main(int argc,char ** argv){
-    
+    char buffer[20];
+    signal(SIGINT,ctrlc);
+    printf("%s[MAIN][%d]%s\n",GREEN,getpid(),DF);    
+    pid[0] = getpid();
 
+
+    while(1){
+        read(STDIN_FILENO,buffer,20);
+        if(isString(buffer)==0){
+            //is number
+            printf("is number\n");
+        }else{
+            //is string
+            printf("is string\n");
+        }
+
+    }
 
     return 0;
 }
