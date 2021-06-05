93741ba5b606af615f7a7e9da433c1751563446b
carlo bottaro <29379533+bottarocarloo@users.noreply.github.com>
2021-06-05 18:20:28 +0200

    message dispatcher

--- LabOS02/esercizi2/treeQueue.c
+++ LabOS02/esercizi2/treeQueue.c
@@ -27,7 +27,7 @@
 
 extern int errno;
 int id;
-int pid[MAX_DEPHT],children[MAX_DEPHT],numChildren;
+int pid[MAX_DEPHT],children[MAX_DEPHT],numChildren,father;
 
 char buffer[4];
 
@@ -37,6 +37,16 @@ struct msg_buffer
     long mtype;
 }msgS,msgR;
 
+void loop(){
+    while(1);
+}
+
+void tab(int x){
+    for(int i =0;i<x;i++){
+        printf("\t");fflush(NULL);
+    }
+}
+
 void quit(){
     for(int i =1;i<MAX_DEPHT;i++){
         printf("%s[%d] killed!%s\n",RED,pid[i],DF);
@@ -45,8 +55,17 @@ void quit(){
     while(wait(NULL)>0);
 }
 
+void print(int sigNum){
+    for(int i =0;i<numChildren;i++){
+        tab(id);
+        printf("%s[ID %d - Parent %d] depht %d%s\n",GREEN,children[i],father,id,DF);
+    }
+}
+
 int main(int argc,char ** argv){
-    int level,r;
+    signal (SIGUSR1, print);
+    int r;
+    long level;
     creat("/tmp/tree",0777);
 	key_t k = ftok("/tmp/tree",1);
 	int queueId=msgget(k,0777|IPC_CREAT);
@@ -59,6 +78,7 @@ int main(int argc,char ** argv){
 
     pid[0] = getpid();
     for(int i=1;i<MAX_DEPHT;i++){
+        father = getpid();
         pid[i] = fork();
         if(getpid()!=pid[0]){
             setpgid(0,getpid());
@@ -75,11 +95,7 @@ int main(int argc,char ** argv){
             read(STDIN_FILENO,buffer,3);
             if(buffer[0]=='q'){
                 /* quit */
-                for(int i =1;i<MAX_DEPHT;i++){
-                    printf("%s[%d] killed!%s\n",RED,pid[i],DF);
-                    kill(pid[i],SIGTERM);   
-                }
-                
+                quit();
                 msgctl(queueId, IPC_RMID, NULL);
                 exit(0);
             }else if(buffer[0]=='k' || buffer[0]=='c'){
@@ -94,6 +110,11 @@ int main(int argc,char ** argv){
                 }
             }else if(buffer[0] == 'p'){
                 //printa
+                for(int i =1;i<MAX_DEPHT;i++){
+                    tab(i);
+                    printf("%s[ID %d - Parent %d] depht %d%s\n",GREEN,pid[i],pid[i-1],i,DF);
+                    kill(pid[i],SIGUSR1);
+                }
             }else{
                 printf("Something went wrong...\n");
             }
@@ -101,12 +122,16 @@ int main(int argc,char ** argv){
     }else{
         while(1){
             msgrcv(queueId,&msgR,sizeof(msgR.mtext),0,id);
-            
             switch (msgR.mtext[0])
             {
             case 'c':
                 /* create */
                 printf("[%d] create..\n",getpid());
+                children[numChildren] = fork();
+                if(children[numChildren] == 0){
+                    loop();
+                }
+                numChildren++;
                 break;
             case 'k':
                 /* kill */
