91cbc7c6b646b55ef0187e8e8fe3d7a8ad1b2995
carlo bottaro <29379533+bottarocarloo@users.noreply.github.com>
2021-07-01 08:58:44 +0200

    ()

--- git.sh
+++ git.sh
@@ -3,5 +3,5 @@ echo Commit name
 read $COMMIT
 cd /Users/BandaBottaro/Documents/Università/SistemiOperativi
 git add *
-git commit -m "'($COMMIT)'"
+git commit -m "($COMMIT)"
 git push origin master 
