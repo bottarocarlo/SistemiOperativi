af740007264ee4cd398378e6f57e4c2f51fd0056
carlo bottaro <29379533+bottarocarloo@users.noreply.github.com>
2021-07-01 08:57:29 +0200

    $COMMIT

--- git.sh
+++ git.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
-
+echo Commit name
+read $COMMIT
 cd /Users/BandaBottaro/Documents/Università/SistemiOperativi
 git add *
-git commit -m "message dispatcher"
+git commit -m '$COMMIT'
 git push origin master 
